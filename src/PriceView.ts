e23cbd6fbbff162e1002eba763841304a4f8f078
Johnny Dickson <johnnydicksoncode@gmail.com>
2021-11-10 15:14:01 -0600

    Implement ProductCatalogView.ts

--- src/PriceView.ts
+++ src/PriceView.ts
@@ -1,4 +1,18 @@
 
 export class PriceView {
 
+    /**
+     * Displays a product's total price (Item Unit Price + Qty)
+     */
+    displayProductTotalPrice(){
+
+    }
+
+
+    /**
+     * Displays the total to user
+     */
+    displayTotalPrice() {
+
+    }
 }
