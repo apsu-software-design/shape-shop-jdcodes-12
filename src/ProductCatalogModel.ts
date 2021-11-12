375b65cd69b348c602a32db444a98b45d00e5a31
Johnny Dickson <johnnydicksoncode@gmail.com>
2021-11-11 21:25:55 -0600

    Implement ProductCatalogModel.ts

--- src/ProductCatalogModel.ts
+++ src/ProductCatalogModel.ts
@@ -8,26 +8,49 @@ export class ProductCatalogModel {
     }
 
     addProductToCatalog(product: ProductModel) : void {
-
+        this.productCatalog.push(product)
     }
 
     removeProductFromCatalog(product: ProductModel) : void {
-
+        this.removeProduct(product)
     }   
     
-    updateProductNameInCatalog(product: ProductModel) : void  {
-
+    updateProductNameInCatalog(product: ProductModel, newName: string) : void  {
+        let productToFind = product.getName().toLowerCase()
+        for(let i=0; i < this.productCatalog.length; i++){
+            if(this.productCatalog[i].getName().toLowerCase() === productToFind)
+            {
+                this.productCatalog[i].setName(newName);
+            }
+        }
     }
 
-    updateProductPriceInCatalog(product: ProductModel) : void {
+    updateProductPriceInCatalog(product: ProductModel, newPrice: number) : void {
+        let productToFind = product.getName().toLowerCase()
+        for(let i=0; i < this.productCatalog.length; i++){
+            if(this.productCatalog[i].getName().toLowerCase() === productToFind){
 
+                this.productCatalog[i].setPrice(newPrice)
+            }
+        }
     }
 
-    updateProductDescriptionInCatalog(product: ProductModel) : void{
+    updateProductDescriptionInCatalog(product: ProductModel, newDescription : string) : void{
+        let productToFind = product.getName().toLowerCase()
+        for(let i=0; i < this.ProductCatalogModel.length; i++){
+            if(this.productCatalog[i].getName().toLowerCase() === productToFind){
 
+                this.productCatalog[i].setDescription(newDescription)
+            }
+        }
     }
 
     clearProductCatalog() : void {
+        this.productCatalog.splice(0)
+    }
 
+    private removeProduct(product: ProductModel) {
+        this.productCatalog.forEach((value,index)=>{
+            if(value==product) this.productCatalog.splice(index,1)})
     }
 }
