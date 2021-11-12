8964632d32f61e976aa7febaa0fe0f141f6852dd
Johnny Dickson <johnnydicksoncode@gmail.com>
2021-11-11 21:52:16 -0600

    Implement PriceModel.ts

--- src/PriceModel.ts
+++ src/PriceModel.ts
@@ -15,23 +15,50 @@ export class PriceModel {
     }
     
     calculateSubtotals(): void {
-        
+        this.shoppingCart.getShoppingCart().forEach(this.calcAllSubtotals(this.shoppingCart))
     }
 
     calculateTotal() : void {
-
+        this.subtotals.forEach((subtotal) => this.total += subtotal)
     }
 
-    adjustSubtotal(product: ProductModel) : void {
+    //Inefficient
+    adjustSubtotal(product: ProductModel, productUnitPrice: number, quantityBuying: number) : void {
+        let productToFind = product.getName().toLowerCase()
+        for(let i=0; i < this.shoppingCart.getShoppingCart().length; i++){
 
+            if(this.shoppingCart.getShoppingCart()[i].getName().toLowerCase() === productToFind){
+
+                let foundProudct = this.shoppingCart.getShoppingCart()[i]
+                foundProudct.setPrice(productUnitPrice)
+                foundProudct.setQuantity(quantityBuying)
+
+                this.calcAllSubtotals(this.shoppingCart) 
+            }
+        }
     }
     
     clearSubtotals() : void {
-
+        this.subtotals.forEach((subtotal)=> this.total = 0.0)
     }
 
     resetTotal() : void {
         this.total = 0.0
+    }
+
+    private calcAllSubtotals(shoppingCart: ShopppingCartModel) : any {
+        
+        if(this.subtotals.length !== 0){
+            this.subtotals.splice(0)
+        }
+        else{
+            for(let i=0; i < shoppingCart.getShoppingCart().length; i++){
+                let sub = (shoppingCart.getShoppingCart()[i].getPrice() * 
+                                shoppingCart.getShoppingCart()[i].getQuantity())
+            
+                this.subtotals.push(sub)
+            }
+        }   
     }
 
 }
--- src/ShoppingCartView.ts
+++ src/ShoppingCartView.ts
@@ -10,7 +10,7 @@ export class ShoppingCartView {
     displayCurrentCartState() {
         console.log("Current Items In Cart...")
         for(let i=0; i < this.shoppingCart.getShoppingCart().length; i++){
-            console.log(`Name: ${this.shoppingCart. getShoppingCart()[i].getName()}`)
+            console.log(`Name: ${this.shoppingCart.getShoppingCart()[i].getName()}`)
             console.log(`Price: ${this.shoppingCart.getShoppingCart()[i].getPrice()}`)
             console.log(`Quantity: ${this.shoppingCart.getShoppingCart()[i].getQuantity()}`)
         }
